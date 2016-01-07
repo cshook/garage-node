68c79b59c6a83119ad024374919bfee4fe0dfe2d
Craig Shook <cshook@assetnation.com>
2016-01-06 20:48:51 -0500

    change pin settings

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ var config = {};
 config.SENSOR_PIN = 0;
 config.RELAY_PIN = 18;
 
-config.RELAY_ON = 1;
-config.RELAY_OFF = 0;
+config.RELAY_ON = 0;
+config.RELAY_OFF = 1;
 config.RELAY_TIMEOUT = 500;
 
 module.exports = config;
